c905303368dc5d00d3c41e527f120553f1222e74
Alejandro Pestana <alejandropestana@outlook.com>
2022-05-09 12:11:57 -0400

    Agregar archivos de configuración y carpetas
    Agregar archivos:
    
    * package.json que contiene la metadata del proyecto
    * package-lock.json que se genera automáticamente para tener registro de los paquetes
    * .gitignore para omitir archivos de configuración y módulos de node (cada quién debe instalarlos por su cuenta)
    * tsconfig.json para establecer los directorios y las opciones para compilar el proyecto
    * test.ts y test.js para probar y agregar las carpetas src y dist. Se pueden eliminar después
    
    Agregar carpetas:
    
    * src para los archivos TypeScript
    * dist para los archivos compilados JavaScript

---
+++ dist/test.js
@@ -0,0 +1,6 @@
+"use strict";
+function foo(bar) {
+    return 'Hola, ' + bar;
+}
+let baz = 'mundo';
+console.log(foo(baz));
---
+++ src/test.ts
@@ -0,0 +1,7 @@
+function foo(bar: string) {
+    return 'Hola, ' + bar
+}
+
+let baz = 'mundo'
+
+console.log(foo(baz))
