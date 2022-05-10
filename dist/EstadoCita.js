56ac7b86162176821200e169dd35a2d936f8fe1c
miguelcuadrado21 <miguelcuadrado2112@gmail.com>
2022-05-09 20:14:06 -0400

    Eliminación de archivos viejos
    Se eliminaron 2 compilaciones viejas que sobran.

--- dist/EstadoCita.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var EstadoCita;
-(function (EstadoCita) {
-    EstadoCita["CONFIRMADA"] = "CONFIRMADA";
-    EstadoCita["CANCELADA"] = "CANCELADA";
-    EstadoCita["PENDIENTE"] = "PENDIENTE";
-})(EstadoCita || (EstadoCita = {}));
--- dist/TipoCita.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var TipoCita;
-(function (TipoCita) {
-    TipoCita["TELECONSULTA"] = "TELECONSULTA";
-    TipoCita["PRESENCIAL"] = "PRESENCIAL";
-})(TipoCita || (TipoCita = {}));
